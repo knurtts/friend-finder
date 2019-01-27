cd570a6bfd5e634e6fe7403be083f76c7f0a44c1
Robert <robertwinstonanderson@gmail.com>
2019-01-26 17:56:49 -0700

    add logic for match alg

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,15 +1,34 @@
+var friends = require("../data/friends");
+
 module.exports = function(app) {
-    const friends = require("../data/friends");
 
     app.get("/api/friends", function(req, res) {
         res.json(friends);
      });
      
-     app.post("api/friends", function(req, res) {
+     app.post("/api/friends", function(req, res) {
          var newFriend = req.body;
-     
-         console.log(newFriend);
-         friends.push(newFriend);
-         res.json(true);
-     });
-}
+         var match;
+         var totalDiff = 0;
+         var diffArray = [];
+         for (let i = 0; i < friends.length; i++) {
+            var answers = friends[i].scores;
+            for (let i = 0; i < answers.length; i++) {
+              totalDiff += Math.abs(answers[i] - newFriend.scores[i]);
+            }
+            diffArray.push(totalDiff);
+        }
+
+        var value = 10;
+        var candidate = 0;
+        for (let i = 0; i < diffArray.length; i++) {
+            if (diffArray[i] < value) {
+                value = diffArray[i];
+                candidate = i;
+            }
+        }
+        console.log("Your match is " + match.name);
+        friends.push(newFriend);
+        return res.json(newFriend);
+    });
+};
